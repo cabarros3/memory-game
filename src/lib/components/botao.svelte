17e31831f6270fc9abec1e27e99a7b5fe408fd78
Camilla Barros <camillaerbarros@gmail.com>
2025-06-29 15:41:31 -0300

    feat: implementa componente botão

---
+++ src/lib/components/botao.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let text = "Jogar";
+  export let onClick = () => {};
+</script>
+
+<div>
+    <button class="px-5 text-lg courier-prime-bold bg-color3 text-black rounded-lg hover:bg-color2 shadow-md" type="button" on:click={onClick}>
+            {text}
+    </button>   
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<a href="/play">Jogar</a>
-<a href="/about">Sobre</a>
+<script lang="ts">
+  import '../app.css';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+	import Botao from '../lib/components/botao.svelte';
+
+  let animate = false;
+
+  onMount(() => {
+    animate = true;
+    setTimeout(() => (animate = false), 5000); // duração de 1s
+  });
+ 
+
+  function jogar() {
+    goto('/play');
+  }
+  
+  function sobre(){
+    goto('/about')
+  }
+</script>
+
+<main class="flex flex-col justify-center items-center my-24">
+
+  <div>
+      <div class="flex flex-row gap-2 items-center">
+        <img src="/logo.png" alt="Logo do jogo" width="50px" height="auto" class:spin-once={animate}>
+        <h1 class="text-3xl courier-prime-bold">Memory Game</h1>
+      </div>
+  </div>
+
+<div class="flex flex-col gap-5 items-center justify-center my-5">
+  <Botao text="Play" onClick={jogar} />
+  <Botao text="About" onClick={sobre} />
+</div>
+</main>
