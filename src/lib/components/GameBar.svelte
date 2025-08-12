da362de80c1246c9c66f3f80657f1465e518fa7c
Camilla Barros <camillaerbarros@gmail.com>
2025-08-11 21:22:15 -0300

    refactor: ajusta detalhes da ui do jogo

--- src/lib/components/GameBar.svelte
+++ src/lib/components/GameBar.svelte
@@ -2,7 +2,7 @@
   import { createEventDispatcher } from "svelte";
   import { page } from '$app/stores';
   import { get } from 'svelte/store';
-  import { DoorOpen, Pause, Play, Settings } from 'lucide-svelte';
+  import { LogOut, Pause, Play } from 'lucide-svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -11,6 +11,7 @@
   export let tempo: string = "00:00";
   export let nivel: number = 1;
   export let tentativas: number = 0;
+  export let acertos: number = 0
   export let instructions: string = "Need help?"
   export let jogoPausado: boolean = false;
 
@@ -44,18 +45,23 @@
     {/if}
   </button>
 
- <div class="flex bg-color2 text-black px-5 py-2 shadow-lg text-sm justify-center items-center rounded-2xl w-[4.5rem]">
+ <div class="flex bg-green-600 text-black px-5 py-2 shadow-lg text-sm justify-center items-center rounded-2xl w-[4.5rem]">
   <span class="font-mono">{tempo}</span>
 </div>
-  <div class="bg-color1 px-5 text-sm shadow-lg flex justify-center items-center rounded-2xl">
+  <div class="bg-color1 px-5 text-sm shadow-lg flex flex-col justify-center items-center rounded-2xl">
+    <span>Tentativas</span>
     <span>{tentativas}</span>
+  </div>
+  <div class="bg-color2 px-5 text-sm shadow-lg flex flex-col justify-center items-center rounded-2xl">
+    <span>Acertos</span>
+    <span>{acertos}</span>
   </div>
    {#if !pathname.includes('/adventure')}
     <div class="bg-color3 px-5 py-2 text-sm shadow-lg flex justify-center items-center rounded-2xl">
     <span>Level {nivel}</span>
   </div>
   {/if}
-  <button class="bg-color1 px-5 text-sm shadow-lg flex justify-center items-center rounded-2xl hover:cursor-pointer" on:click={handleAjudaClick}>{instructions}</button>
+  <button class="bg-gray-200 px-5 text-sm shadow-lg flex justify-center items-center rounded-2xl hover:cursor-pointer" on:click={handleAjudaClick}>{instructions}</button>
   
 
   <!-- Botão Sair -->
@@ -64,7 +70,7 @@
     on:click={handleExit}
     aria-label="Exit game"
   >
-    <Settings />
+    <LogOut />
   </button>
   
 </div>
